032da9b3a172e807c8b01bc3bad1ca2143798544
Apramay <142184694+Apramay@users.noreply.github.com>
2025-04-02 16:09:16 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,7 +1,6 @@
 const suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
 const ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
 const rankValues = { "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10, "J": 11, "Q": 12, "K": 13, "A": 14 };
-import * as solanaWeb3 from "@solana/web3.js";
 
 // Function to get URL parameters
 function getQueryParam(param) {
